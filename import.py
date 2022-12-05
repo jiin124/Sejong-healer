836f5a2ce9dba4399c27f5b66c59da712f47e8b3
isakacindy <94227598+isakacindy@users.noreply.github.com>
2022-12-05 16:23:11 +0900

    Delete import.py

--- import.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-from sklearn.linear_model import Lasso
-from sklearn.linear_model import LassoCV
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, LabelEncoder
